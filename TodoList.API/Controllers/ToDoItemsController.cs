33dcd9f8962f80a7ecbbf9fc8fe681aa45f1b5f6
Gurdeep Singh <A00259174@mycambrian.ca>
2024-02-14 16:26:45 -0500

    Add project files.

---
+++ TodoList.API/Controllers/ToDoItemsController.cs
@@ -0,0 +1,30 @@
+﻿// GET: api/ToDoItems
+using Microsoft.AspNetCore.Mvc;
+
+[HttpGet]
+public async Task<ActionResult<IEnumerable<ToDoItem>>> GetIncompleteItems([FromQuery] int pageNumber, [FromQuery] int pageSize)
+{
+    try
+    {
+        var incompleteItems = await _context.ToDoItems
+          .AsNoTracking() // for performance
+          .Where(i => i.CompletedDate == null)
+          .Skip(pageNumber * pageSize) // paging
+          .Take(pageSize) // paging
+          .ToListAsync();
+
+        if (!incompleteItems.Any())
+        {
+            return NotFound();
+        }
+
+        return incompleteItems;
+
+    }
+    catch (Exception ex)
+    {
+        // log error
+
+        return StatusCode(500, "Error retrieving incomplete items");
+    }
+}
---
+++ TodoList.Tests/ToDoItemTests.cs
@@ -0,0 +1,58 @@
+﻿// Test 1 
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+[Test]
+public void TestToDoItemConstructor()
+{
+    // Arrange
+    int expectedId = 1;
+    DateTime expectedDueDate = new DateTime(2023, 1, 1);
+
+    // Act
+    var item = new ToDoItem(expectedId, expectedDueDate);
+
+    // Assert 
+    Assert.AreEqual(expectedId, item.Id);
+    Assert.AreEqual(expectedDueDate, item.DueDate);
+}
+
+// Test 2
+[Test]
+public void CompletedDateIsNullByDefault()
+{
+    // Arrange
+    var item = new ToDoItem(1, DateTime.Now);
+
+    // Assert
+    Assert.IsNull(item.CompletedDate);
+}
+
+// Test 3
+[Test]
+public void CanCompleteToDoItem()
+{
+    // Arrange 
+    var item = new ToDoItem(1, DateTime.Now);
+
+    // Act
+    item.Complete();
+
+    // Assert
+    Assert.IsNotNull(item.CompletedDate);
+}
+
+// Test 4
+[Test]
+public void CanUncompleteToDoItem()
+{
+    // Arrange
+    var item = new ToDoItem(1, DateTime.Now);
+    item.Complete();
+
+    // Act
+    item.Uncomplete();
+
+    // Assert
+    Assert.IsNull(item.CompletedDate);
+}
